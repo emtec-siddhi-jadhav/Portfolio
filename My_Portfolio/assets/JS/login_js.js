59bcf95f61be302b05f041746ec6f6dd5b07cc9a
Siddhi-Jadhav <97875650+Siddhi-Jadhav@users.noreply.github.com>
2022-02-02 20:19:46 +0530

    Add files via upload

--- My_Portfolio/assets/JS/login_js.js
+++ My_Portfolio/assets/JS/login_js.js
@@ -2,26 +2,21 @@ function login()
 	{
 		var uname = document.getElementById("email").value;
 		var pwd = document.getElementById("pass").value;
-		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if(uname =='')
-		{
-			alert("please enter email");
-		}
-		else if(pwd=='')
-		{
-        	alert("enter the password");
-		}
-		else if(!filter.test(uname))
-		{
-			alert("Enter valid email id.");
-		}
-		else if(pwd.length < 6 || pwd.length > 12)
-		{
-			alert("Password min length is 6 and max length is 12.");
-		}
-		else
+		//var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='1803037@ritindia.edu' && pwd=='rit12345')
 		{
 			window.location = "MyPortfolio.html";
+		}
+		else if(uname!='1803037@ritindia.edu'){
+			document.getElementById('email_error').innerHTML="Please enter a valid mail";  
+		}
+		else if(pwd!='rit12345'){
+			document.getElementById('pass_error').innerHTML="Please enter a correct password";  
+		}
+		
+		else
+		{
 			
+			alert("Wrong Credentials");
 		}
 	}
